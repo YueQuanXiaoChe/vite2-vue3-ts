1e9f9895c3a1a0d313ebe316c524bc0bca559202
wangxue <312759057@qq.com>
2021-03-05 17:13:15 +0800

    vant组件样式按需加载

--- src/views/Demo/index.vue
+++ src/views/Demo/index.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { Button } from "vant";
+import "vant/es/button/style/index";
 
 export default defineComponent({
   components: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,36 @@
-import { defineConfig } from "vite";
+import { UserConfigExport } from "vite";
+import styleImport from "vite-plugin-style-import";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default (): UserConfigExport => {
+  return {
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+        },
+      },
     },
-  },
-});
+    plugins: [
+      vue(),
+      styleImport({
+        libs: [
+          {
+            libraryName: "vant",
+            esModule: true,
+            resolveStyle: (name) => {
+              return `vant/es/${name}/style/index`;
+            },
+          },
+        ],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+};
