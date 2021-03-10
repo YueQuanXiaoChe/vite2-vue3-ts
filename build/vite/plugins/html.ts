7dbbba14f3ea093350e6a6901b2da36c725feec8
wangxue <312759057@qq.com>
2021-03-10 19:46:51 +0800

    引入一个针对 index.html，提供精简和基于 ejs 模板功能的 Vite 插件：vite-plugin-html

---
+++ build/vite/plugins/html.ts
@@ -0,0 +1,46 @@
+/**
+ * Plugin to minimize and use ejs template syntax in index.html.
+ * https://github.com/anncwb/vite-plugin-html
+ */
+import type { Plugin } from 'vite';
+import type { ViteEnv } from '../../utils';
+import { getBuildEnv } from '../../utils';
+
+import html from 'vite-plugin-html';
+
+import pkg from '../../../package.json';
+
+export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
+  const { VITE_GLOB_APP_TITLE } = env;
+  const buildEnv = getBuildEnv();
+
+  const isMinify = (): boolean => {
+    let res = false;
+    if (isBuild && buildEnv !== 'dev' && buildEnv !== 'test') {
+      res = true;
+    }
+    return res;
+  };
+
+  const htmlPlugin: Plugin[] = html({
+    minify: isMinify(),
+    inject: {
+      // Inject data into ejs template
+      injectData: {
+        title: VITE_GLOB_APP_TITLE,
+        version: pkg.version
+      },
+      tags: [
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'application-version',
+            content: pkg.version
+          },
+          injectTo: 'head'
+        }
+      ]
+    }
+  });
+  return htmlPlugin;
+}
--- build/vite/plugins/index.ts
+++ build/vite/plugins/index.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 import { configStyleImportPlugin } from './styleImport';
 import { configVisualizerConfig } from './visualizer';
 import { configCompressPlugin } from './compress';
+import { configHtmlPlugin } from './html';
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const { VITE_BUILD_COMPRESS } = viteEnv;
@@ -14,6 +15,9 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
     // have to
     vue()
   ];
+
+  // vite-plugin-html
+  vitePlugins.push(configHtmlPlugin(viteEnv, isBuild));
 
   // vite-plugin-style-import
   vitePlugins.push(configStyleImportPlugin());
