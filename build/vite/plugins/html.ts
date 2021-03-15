634ad535bf2aa9e929b5d525ba34e72969626985
wangxue <312759057@qq.com>
2021-03-15 16:34:24 +0800

    优化

--- build/vite/plugins/html.ts
+++ build/vite/plugins/html.ts
@@ -31,10 +31,37 @@ export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
     inject: {
       // Inject data into ejs template
       injectData: {
-        title: VITE_GLOB_APP_TITLE,
-        version: pkg.version
+        title: VITE_GLOB_APP_TITLE
       },
       tags: [
+        // 强制Chromium内核，作用于360浏览器、QQ浏览器等国产双核浏览器
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'renderer',
+            content: 'webkit'
+          },
+          injectTo: 'head'
+        },
+        // 强制Chromium内核，作用于其他双核浏览器
+        {
+          tag: 'meta',
+          attrs: {
+            name: 'force-rendering',
+            content: 'webkit'
+          },
+          injectTo: 'head'
+        },
+        // 如果有安装 Google Chrome Frame 插件则强制为Chromium内核，否则强制本机支持的最高版本IE内核，作用于IE浏览器
+        {
+          tag: 'meta',
+          attrs: {
+            'http-equiv': 'X-UA-Compatible',
+            content: 'IE=Edge,chrome=1'
+          },
+          injectTo: 'head'
+        },
+        // 版本号
         {
           tag: 'meta',
           attrs: {
@@ -42,6 +69,45 @@ export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
             content: pkg.version
           },
           injectTo: 'head'
+        },
+        // [icontfont] font-class引用，只支持单色
+        // 预加载外部样式表
+        {
+          tag: 'link',
+          attrs: {
+            href: 'https://at.alicdn.com/t/font_1700262_4p69bg9gl8k.css',
+            rel: 'preload',
+            as: 'style'
+          },
+          injectTo: 'head'
+        },
+        // 定义外部加载的样式表
+        {
+          tag: 'link',
+          attrs: {
+            href: 'https://at.alicdn.com/t/font_1700262_4p69bg9gl8k.css',
+            rel: 'stylesheet'
+          },
+          injectTo: 'head'
+        },
+        // [icontfont] symbol 引用，支持多色
+        // 预加载外部 js
+        {
+          tag: 'link',
+          attrs: {
+            href: 'https://at.alicdn.com/t/font_1700262_4p69bg9gl8k.js',
+            rel: 'preload',
+            as: 'script'
+          },
+          injectTo: 'head'
+        },
+        // 定义外部加载的 js
+        {
+          tag: 'script',
+          attrs: {
+            src: 'https://at.alicdn.com/t/font_1700262_4p69bg9gl8k.js'
+          },
+          injectTo: 'body'
         }
       ]
     }
